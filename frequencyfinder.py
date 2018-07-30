d174d623ad60f9eaeca99a4ca2abd45cfd54fb05
Aaron <joseyvalerio@gmail.com>
2018-07-30 10:08:29 -0400

    Frequency finder was added

---
+++ frequencyfinder.py
@@ -0,0 +1,13 @@
+scores = [100,97,45,90,90,76,67,28,398,97,45,90,100,67,80,84,85,86,87,90,90,100]
+
+def frequencyFinder(list):
+    freq = {}
+    for i in list:
+        if i in freq:
+            freq[i] += 1
+        else:
+            freq[i] = 1
+    return freq
+
+print(frequencyFinder(scores))
+
