409b2da945773a283a50389cc5084d4ed0b44e5f
Aaron <joseyvalerio@gmail.com>
2018-07-30 09:02:46 -0400

    Changed list sort to remove duplicates to test mutating lists

--- listsort.py
+++
@@ -1,6 +0,0 @@
-cool = ['a', 'v','d']
-sortedcool = sorted(cool)
-reversecool = list((reversed(sortedcool)))
-print(cool)
-print(sortedcool)
-print(reversecool)
---
+++ removeduplicates.py
@@ -0,0 +1,14 @@
+L1 = [1,2,3,4,5]
+L2 = [1,2,3,4,9]
+
+def remove_dups(L1,L2):
+    L1_Copy = L1[:]
+    for i in L1_Copy:
+        if i in L2:
+            L1.remove(i)
+
+
+remove_dups(L1,L2)
+
+print(L1)
+print(L2)
