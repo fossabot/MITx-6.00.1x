f5b23d8a2a6987d32d7c6a115655e1ac38f6e8d5
Aaron <joseyvalerio@gmail.com>
2018-08-02 00:38:35 -0400

    Fixed some bugs on startup. While loop fixed in the beginning

--- scrabble.py
+++ scrabble.py
@@ -43,87 +43,97 @@ totalPoints = 0
 # PLAY GAME
 userChoice = ''
 userBoard = ''
-
-
-def dealNewHand():
-    userChoice = input(
-        "Enter n to deal a new hand, r to replay the last hand, or e to end the game: ").lower()
-    if userChoice == 'n':
-        print("Deal a new hand")
+acceptableChoices = ['nre']
+def playGame():
+    global playerPiecesCount
+    global totalPoints
+    global userBoard
+    global usedLetters
+    while playerPiecesCount > 0:
         print('')
-        print("New hand, coming right up!")
-        print('')
-        global userBoard
-        userBoard = ''
-        print("Your new hand is: ")
-        print('')
-        for i in range(0, (playerPiecesCount)):  # gets random letter
-            userBoard = userBoard + randomLetter()
-        for i in range(0, len(userBoard)):  # prints userboard
-            print(userBoard[i], ' ', end='')
-
-
-dealNewHand()
-
-
-while playerPiecesCount > 0:
-    print('')
-    playerHand = input(
-        "Enter a word, or a '.' to indicate that you are finished: ").lower()        # while isValidWord(playerHand) == False:
-    if playerHand == ".":
-        print('')
-        print("Ending game. You scored a total of " +
-              str(totalPoints) + " total points.")
-        print('')
-        break
-    elif isValidWord(playerHand):
-        print("")
-        x = 0  # wrong letter counter
-        for i in playerHand.upper():
-            if i not in userBoard:
-                x += 1
-        if x == 0:
-            totalPoints += wordScore(playerHand.upper())
+        playerHand = input(
+            "Enter a word, or a '.' to indicate that you are finished: ").lower()        # while isValidWord(playerHand) == False:
+        if playerHand == ".":
             print('')
-            print("You placed down the word " + "'" + playerHand.upper() + "'" +
-                  " and you scored " + str(wordScore(playerHand.upper())) + " points.")
-            print("You have " + str(totalPoints) + " total points.")
+            print("Ending game. You scored a total of " +
+                str(totalPoints) + " total points.")
+            print('')
+            break
+        elif isValidWord(playerHand):
             print("")
-            print("You have used the letters ")
-            print("")
-            playerHandAdd2Used = playerHand[:]
-            usedLetters += playerHandAdd2Used
-            for i in usedLetters:  # FIX USED LETTERS TODO
-                # prints words that we have used
-                print(i.upper(), ' ', end=' ', flush=True)
+            x = 0  # wrong letter counter
             for i in playerHand.upper():
-                if i in userBoard:   # Removes letters if played
-                    userBoard = userBoard.replace(i, '', 1)
-                playerPiecesCount -= len(playerHand)
-            print("")
+                if i not in userBoard:
+                    x += 1
+            if x == 0:
+                totalPoints += wordScore(playerHand.upper())
+                print('')
+                print("You placed down the word " + "'" + playerHand.upper() + "'" +
+                    " and you scored " + str(wordScore(playerHand.upper())) + " points.")
+                print("You have " + str(totalPoints) + " total points.")
+                print("")
+                print("You have used the letters ")
+                print("")
+                playerHandAdd2Used = playerHand[:]
+                usedLetters += playerHandAdd2Used
+                for i in usedLetters:  # FIX USED LETTERS TODO
+                    # prints words that we have used
+                    print(i.upper(), ' ', end=' ', flush=True)
+                for i in playerHand.upper():
+                    if i in userBoard:   # Removes letters if played
+                        userBoard = userBoard.replace(i, '', 1)
+                    playerPiecesCount -= len(playerHand)
+                print("")
+                print("")
+                print("Your hand is: ")
+                print("")
+                for i in userBoard:  # Prints user hand after incorrect entry
+                    print(i, ' ', end='')
+            else:
+                print(
+                    "Your word was in the dictionary but you don't have sufficient letters so it does not count.")
             print("")
             print("Your hand is: ")
             print("")
             for i in userBoard:  # Prints user hand after incorrect entry
                 print(i, ' ', end='')
+
         else:
-            print(
-                "Your word was in the dictionary but you don't have sufficient letters so it does not count.")
-        print("")
-    elif userChoice == 'r':
-        print("Redoo the last hand")
-    elif userChoice == 'e':
-        print('')
-        print('')
-        print("Game Over")
-        print('')
-        print('')
-        print("Thanks for playing!")
-        break
-    else:
-        print("I am sorry, that is not a valid entry. Please try again.")
-        print("")
-        print("Your hand is: ")
-        print("")
-        for i in userBoard:  # Prints user hand after incorrect entry
-            print(i, ' ', end='')
+            print("I am sorry, that is not a valid entry. Please try again.")
+            print("")
+            print("Your hand is: ")
+            print("")
+            for i in userBoard:  # Prints user hand after incorrect entry
+                print(i, ' ', end='')
+
+def dealNewHand():
+    while True:
+        userChoice = input(
+"Enter n to deal a new hand, r to replay the last hand, or e to end the game: ").lower()
+        if userChoice == 'n':
+            print("Deal a new hand")
+            print('')
+            print("New hand, coming right up!")
+            print('')
+            global userBoard
+            userBoard = ''
+            print("Your new hand is: ")
+            print('')
+            for i in range(0, (playerPiecesCount)):  # gets random letter
+                userBoard = userBoard + randomLetter()
+            for i in range(0, len(userBoard)):  # prints userboard
+                print(userBoard[i], ' ', end='')
+            playGame()
+            break
+        elif userChoice == 'r':
+            print("Redoo the last hand")
+        elif userChoice == 'e':
+            print('')
+            print('')
+            print("Game Over")
+            print('')
+            print('')
+            print("Thanks for playing!")
+            break
+
+dealNewHand()
