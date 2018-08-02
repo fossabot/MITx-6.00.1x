1160be5a999b79a5040a63c7e96b77ed33612cc7
Aaron <joseyvalerio@gmail.com>
2018-08-02 12:13:03 -0400

    Moved some Proof of Concepts to their own folder

--- ccbalance.py
+++ ccbalance.py
@@ -1,6 +1,18 @@
-bal = float((input("What is your credit card balance?")))
-rate = float((input("What is your credit card interest rate?")))
-mPay = float((input("What is your average monthly payment?")))
+bal = input("What is your credit card balance?")
+while bal!= int or bal != float:
+    print("That is not a valid entry.")
+    if:
+        bal == 
+
+try:
+    rate = float((input("What is your credit card interest rate?")))
+except ValueError:
+    print("That is not a valid entry.")
+
+try:
+    mPay = float((input("What is your average monthly payment?")))
+except ValueError:
+    print("That is not a valid entry.")
 
 
 # Checks how long it will take to pay off a CC
@@ -10,7 +22,7 @@ def calcPayment(balance, rate, mPay):
         balance = (balance-mPay) * (1+((rate/100)/12))
         months += 1
     print("It would take you ", str(months), " months to pay off a balance of $", str(
-        bal), " while making monthly payments of $", str(mPay), " at an interest rate of ", str(rate), "%.")
+        bal), " while making monthly payments of $", str(mPay), " at an interest rate of ", str(rate), "%.", sep='')
 
 
 calcPayment(bal, rate, mPay)
