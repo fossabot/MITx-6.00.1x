d0b70bb956a3cfd4ccfd7ac089c12576bb86b8fb
Aaron <joseyvalerio@gmail.com>
2018-08-02 11:11:54 -0400

    Fixed formatting, got rid of +'s

--- ccbalance.py
+++ ccbalance.py
@@ -2,11 +2,15 @@ bal = float((input("What is your credit card balance?")))
 rate = float((input("What is your credit card interest rate?")))
 mPay = float((input("What is your average monthly payment?")))
 
-def calcPayment(balance,rate,mPay):
+
+# Checks how long it will take to pay off a CC
+def calcPayment(balance, rate, mPay):
     months = 0
     while balance > 0:
         balance = (balance-mPay) * (1+((rate/100)/12))
         months += 1
-    print("It would take you " + str(months) + " months to pay off a balance of " + "$" + str(bal) + " while making monthly payments of " + "$" + str(mPay) + " at an interest rate of " + str(rate) + "%.")
+    print("It would take you ", str(months), " months to pay off a balance of $", str(
+        bal), " while making monthly payments of $", str(mPay), " at an interest rate of ", str(rate), "%.")
 
-calcPayment(bal,rate,mPay)
+
+calcPayment(bal, rate, mPay)
